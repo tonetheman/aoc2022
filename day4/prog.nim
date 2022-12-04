299ae54259a163094dbfd5a9a63baf1192544b78
tonethemna <tonetheman@gmail.com>
2022-12-04 08:05:32 -0600

    made a guess and it was wrong and too low not sure what is wrong yet

--- day4/prog.nim
+++ day4/prog.nim
@@ -3,7 +3,9 @@ import strutils
 import std/sets
 
 proc p1() =
-    for line in lines("sample.txt"):
+    var fully_contained = 0
+
+    for line in lines("input.txt"):
         let data = line.split(",")
         let d1 = data[0].split("-")
         let d2 = data[1].split("-")
@@ -17,8 +19,19 @@ proc p1() =
         
         echo(s1)
         echo(s2)
+        if s1 < s2:
+            echo("s1 fully contained")
+            fully_contained += 1
+        if s2 < s1:
+            echo("s2 fully contained")
+            fully_contained += 1
+
         echo()
 
-        break
+    echo("full contained count: ",fully_contained)
+
+    # 569 is not the correct answer?
+    # but it is someone elses answer?
+    # 569 is too low
 
 p1()
