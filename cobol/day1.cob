2abfb594fafdf66e16bdee6bb4af6b7d2abee6e9
tonethemna <tonetheman@gmail.com>
2022-12-05 12:48:44 -0600

    updated

--- cobol/day1.cob
+++ cobol/day1.cob
@@ -4,26 +4,26 @@
         ENVIRONMENT DIVISION.
             INPUT-OUTPUT SECTION.
                 FILE-CONTROL.
-                SELECT STUDENT ASSIGN TO './input.txt'
+                SELECT CFILE ASSIGN TO './input.txt'
                 ORGANIZATION IS LINE SEQUENTIAL.
 
         DATA DIVISION.
-            FILE SECTION.
-            FD STUDENT.
-            01 STUDENT-FILE.
-                05 STUDENT-ID PIC 9(6).
 
             Working-Storage Section.
-            01 WS-STUDENT.
-                05 STUDENT-ID PIC 9(6).
+            01 WS-CALORIES.
+                05 CALORIES PIC 9(6).
+            01 WS-EOF   PIC A(1).
 
         PROCEDURE DIVISION.
             DISPLAY "day1".
-            OPEN input STUDENT.
-                READ STUDENT into WS-STUDENT
-                    DISPLAY WS-STUDENT
-                
-            CLOSE STUDENT.
+            OPEN input CFILE.
+                PERFORM UNTIL WS-EOF='Y'
+                    READ CFILE into WS-CALORIES
+                        AT END MOVE 'Y' to WS-EOF
+                        NOT AT END DISPLAY WS-CALORIES
+                    END-READ
+                END-PERFORM               
+            CLOSE CFILE.
         STOP RUN.
 
         
