31a8e011633dfcc1f92c163299db82944f4c47c4
tonethemna <tonetheman@gmail.com>
2022-12-07 07:06:15 -0600

    saving work

--- day7/prog.nim
+++ day7/prog.nim
@@ -8,12 +8,34 @@ proc makefilebuffer(inputfile:string) : seq[string] =
         buffer.add(line)
     return buffer
 
+type
+    SantaFile = object
+        directory : string
+        name : string
+        size : int
 
 let data = makefilebuffer("sample.txt")
+
+# assume we start at root
+var current_dir = "/"
+var allfiles : seq[SantaFile]
+
 for line in data:
     if line.startsWith("$"):
         # command!
-        discard
+        let cdata = line.split(" ")
+        if cdata[0] != "$":
+            echo("ERR: invalid command!",line)
+            system.quit(-1)
+        if cdata[1] == "cd":
+            if cdata[2] == "/":
+                # switched to outermost root
+                current_dir = "/"
+            else:
+                # went to a directory down or up!
+                discard
+        elif cdata[1] == "ls":
+            discard                
     else:
         # result I think
         discard
