a054b9966db22c8bb832d82808d54a8f0169593a
tonethemna <tonetheman@gmail.com>
2022-12-08 10:11:39 -0600

    part1 working

--- day8/prog.nim
+++ day8/prog.nim
@@ -7,7 +7,7 @@ proc makefilebuffer(inputfile:string) : seq[string] =
         buffer.add(line)
     return buffer
 
-proc fileseq(data : seq[string]) : seq[ seq[int] ] =
+proc creategrid(data : seq[string]) : seq[ seq[int] ] =
     let rows = len(data)
     let cols = len(data[0])
 
@@ -25,23 +25,82 @@ proc fileseq(data : seq[string]) : seq[ seq[int] ] =
 
 proc vis(a : seq[ seq[int] ], r,c : int) : bool = 
     let me = a[r][c]
-    # check left
     var ll = 0
+    var good : bool
+
+    good = true
+    ll = 0
     while ll < c:
-        echo("checking ",r, " ",ll, a[r][ll])
         if a[r][ll] < me:
             ll += 1
+            continue
         else:
-            return false
-    return true
+            good = false
+            break
+    # echo("good chaeck Left: ",good)
+    if good:
+        return true
+
+    good = true
+    ll = 0
+    while ll < r:
+        if a[ll][c]<me:
+            ll += 1
+            continue
+        else:
+            good = false
+            break
+    # echo("good value for top check: ",good)
+    if good:
+        return true
+
+    good = true
+    ll = len(a[0])-1
+    while ll>c:
+        # echo("R:checking: ",r, " ",ll," ",a[r][ll])
+        if a[r][ll]<me:
+            ll -= 1
+            continue
+        else:
+            good = false
+            break
+    # echo("GOOD value for RIGHT CHECK: ", good)
+    if good: return true
+
+    good = true
+    ll = len(a)-1
+    while ll>r:
+        # echo("B:checking: ",ll," ",c," ",a[ll][c])
+        if a[ll][c]<me:
+            ll -= 1
+            continue
+        else:
+            good = false
+            break
+    # echo("GOOD VALUE FOR B check: ", good)
+    if good: return true
+    
+
+proc count(a : seq[ seq[int] ]) : int =
+    let rows = len(a)
+    let cols = len(a[0])
+
+    # outside ring first
+    var res = 2*rows + 2*(cols-2)
+
+    # now count inside trees
+    for i in 1 .. cols-2:
+        for j in 1 .. rows-2:
+            if vis(a,i,j):
+                res += 1
+    return res
+
 
 # data from file
-let data = makefilebuffer("sample.txt")
+let data = makefilebuffer("input.txt")
 echo(data)
-# rows and cols
-let rows = len(data)
-let cols = len(data[0])
+
 # create the grid
-let a = fileseq(data)
-echo(a)
-echo(vis(a,1,1))
+let a = creategrid(data)
+let res = count(a)
+echo("res: ",res)
