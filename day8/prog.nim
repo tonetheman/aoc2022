16eff73209f7f1e2a23efc0abbf7cb524a3a1995
tonethemna <tonetheman@gmail.com>
2022-12-08 11:00:01 -0600

    changed to use a module

--- day8/prog.nim
+++ day8/prog.nim
@@ -1,27 +1,5 @@
 
-import strutils
-
-proc makefilebuffer(inputfile:string) : seq[string] =
-    var buffer : seq[string]
-    for line in lines(inputfile):
-        buffer.add(line)
-    return buffer
-
-proc creategrid(data : seq[string]) : seq[ seq[int] ] =
-    let rows = len(data)
-    let cols = len(data[0])
-
-    var a : seq[ seq[int] ] = newSeq[newSeq[int](cols)](rows)
-    var rowindex = 0
-    for line in data:
-        var colindex = 0
-        for c in line:
-            # WTF why did this not work?
-            # a[rowindex][colindex] = ord(c)-ord('0')
-            a[rowindex].add(ord(c)-ord('0'))
-            colindex += 1
-        rowindex += 1
-    return a
+import tonyutils
 
 proc vis(a : seq[ seq[int] ], r,c : int) : bool = 
     let me = a[r][c]
