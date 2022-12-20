695e2344b2838e39d4a132cbe431cfb2604a0147
tonethemna <tonetheman@gmail.com>
2022-12-20 08:37:31 -0600

    saving not working mod math is hard hahaha

---
+++ day20/prog.nim
@@ -0,0 +1,108 @@
+
+import tonyutils
+import strutils
+
+type
+    Node = object
+        val : int
+        snum : int
+
+proc part1() =
+    var fdata = makefilebuffer("sample.txt")
+    var data : seq[Node] = newSeq[Node](len(fdata))
+    for i in 0 ..< len(fdata):
+        data[i].snum = i
+        data[i].val = parseInt(fdata[i])
+
+    echo(data)
+
+    proc find_snum_index(target_snum : int) : int =
+        for i in 0 ..< len(data):
+            if data[i].snum == target_snum:
+                return i
+        # if you got here there is a problem
+        system.quit(-1)
+
+    let ll = len(data)
+    for i in 0 ..< len(data):
+        echo("working on i: ",i)
+
+        # find the orig Node with snum == i
+        let target_snum = i
+        var index = find_snum_index(target_snum)
+        echo("\tindex is: ",index)
+
+        var val = data[index]
+        if val.val == 0:
+            continue
+        var num : int
+        num = index + val.val + 1
+        if val.val<0:
+            num = index + (ll-val.val+1)
+        echo("\t",val," ",num)
+        data.insert(val,num mod ll)
+        data.delete(index)
+        
+
+        # move it in the list
+
+        # stop to debug
+        # break
+
+    var ts : string
+    for d in data:
+        ts = ts & " " & $d.val
+    echo(ts)
+
+part1()
+
+proc test1() =
+    var a = @[1,2,-3,3,-2,0,4]
+
+    let ll = len(a)
+    echo("len of a is ",ll)
+
+    # NOTE
+    # this is correct
+    # just need a way to make sure
+    # index is set correctly
+
+    var index = 0 # hard coded
+    var val = a[index]
+    var num : int
+    num = index + val + 1
+    if val<0:
+        num = index + (ll-val+1)
+    a.insert(val,num mod 11)
+    a.delete(index)
+    echo(a)
+
+    index = 0 # hard coded
+    val = a[index]
+    num = index + val + 1
+    if val<0:
+        num = index + (ll-val+1)
+    a.insert(val,num mod ll)
+    a.delete(index)
+    echo(a)
+
+    index = 1 # hard coded
+    val = a[index]
+    num = index + val + 1
+    if val<0:
+        num = index + (ll-val+1)
+    a.insert(val, num mod ll)
+    a.delete(index)
+    echo(a)
+
+    
+# test1()
+
+proc test2() =
+    # checking the math
+    let ll = 3
+    for i in -1 ..< 10:
+        var num = i
+        if i<0:
+            num = ll-i+1
+        echo(num mod ll)
