eb3f438861f273d68f754a61aa64c9169f12eef1
tonethemna <tonetheman@gmail.com>
2022-12-20 08:49:36 -0600

    adjusted

--- day20/prog.nim
+++ day20/prog.nim
@@ -38,69 +38,29 @@ proc part1() =
         var index = find_snum_index(data,target_snum)
         echo("\tindex is: ",index)
 
+        # get copy of orig node
         var val = data[index]
+        # for 0 values do nothing
         if val.val == 0:
             continue
+
         var num : int
+
+        # new position
         num = index + val.val + 1
+        # handle negatives
         if val.val<0:
             num = index + (ll-val.val+1)
+        
         echo("\t",val," ",num)
+
+        # real move and delete here
         data.insert(val,num mod ll)
         data.delete(index)
+        
+        # debug
         echo("\t",data.repr())
         
     echo(data.repr())
 
 part1()
-
-proc test1() =
-    var a = @[1,2,-3,3,-2,0,4]
-
-    let ll = len(a)
-    echo("len of a is ",ll)
-
-    # NOTE
-    # this is correct
-    # just need a way to make sure
-    # index is set correctly
-
-    var index = 0 # hard coded
-    var val = a[index]
-    var num : int
-    num = index + val + 1
-    if val<0:
-        num = index + (ll-val+1)
-    a.insert(val,num mod 11)
-    a.delete(index)
-    echo(a)
-
-    index = 0 # hard coded
-    val = a[index]
-    num = index + val + 1
-    if val<0:
-        num = index + (ll-val+1)
-    a.insert(val,num mod ll)
-    a.delete(index)
-    echo(a)
-
-    index = 1 # hard coded
-    val = a[index]
-    num = index + val + 1
-    if val<0:
-        num = index + (ll-val+1)
-    a.insert(val, num mod ll)
-    a.delete(index)
-    echo(a)
-
-    
-# test1()
-
-proc test2() =
-    # checking the math
-    let ll = 3
-    for i in -1 ..< 10:
-        var num = i
-        if i<0:
-            num = ll-i+1
-        echo(num mod ll)
