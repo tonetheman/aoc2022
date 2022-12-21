42cacd099a9174e0d7e370fd3a96e7599e83dde2
tonethemna <tonetheman@gmail.com>
2022-12-21 13:37:42 -0600

    Saving

--- day21/prog.nim
+++ day21/prog.nim
@@ -61,16 +61,12 @@ proc resolve(name : string, m : var Table[string,Action]) =
             m[name].val = int(m[v1].val / m[v2].val)
             m[name].completed = true
 
-proc resolve2(name : string, m : var Table[string,Action]) =
+proc resolve2(name : string, m : var Table[string,Action]) : int =
     if m[name].completed:
-        return
+        return 0
     if m[name].typ == sInt:
-        return
+        return 0
     
-    # let act = m[name].sact.split(" ")
-    # let op = act[1]
-    # let v1 = act[0]
-    # let v2 = act[2]
     let op = m[name].op
     let v1 = m[name].v1
     let v2 = m[name].v2
@@ -79,7 +75,7 @@ proc resolve2(name : string, m : var Table[string,Action]) =
         if m[v1].completed and m[v2].completed:
             if m[v1].val == m[v2].val:
                 m["root"].completed = true
-        return
+        return 100
 
     if m[v1].completed and m[v2].completed:
         if op == "*":
@@ -94,6 +90,7 @@ proc resolve2(name : string, m : var Table[string,Action]) =
         elif op == "/":
             m[name].val = int(m[v1].val / m[v2].val)
             m[name].completed = true
+    return 0
 
 proc countcompleted(m : Table[string,Action]) : int =
     var count = 0
@@ -126,7 +123,7 @@ proc reset_completed(m : var Table[string,Action]) =
             m[name].completed = false
 
 proc part2() =
-    let fdata = makefilebuffer("input.txt")
+    let fdata = makefilebuffer("sample.txt")
     var m : Table[string,Action]
     for s in fdata:
         insert_new_action(m,s)
@@ -135,27 +132,39 @@ proc part2() =
     let ll = len(m)
 
     # change humn action to a number
-    for i in 20000 ..< 100000:
+    for i in 0 ..< 3:
         # echo("starting humn is: ", m["humn"])
         m.reset_completed()
         m["humn"].val = i
 
         # eval and look for root equality match
-        while m.countcompleted() < ll-1:
+        block outer:
+            while m.countcompleted() < ll-1:
 
-            for singlemonkey in m.keys:
-                resolve2(singlemonkey,m)
+                for singlemonkey in m.keys:
+                    let res = resolve2(singlemonkey,m)
+                    # 100 is a signal from root
+                    # that it did resolve true
+                    # which could happen early?
+                    if res == 100:
+                        echo("breaking to outer")
+                        break outer
+                    
 
         # echo("root completion: ",m["root"].completed)
-        resolve2("root",m)
+        let res = resolve2("root",m)
+        if res == 100:
+            echo("done?",i)
         # echo("root completion: ",m["root"].completed)
         if m["root"].completed == true:
             echo("i is ",i)
             break
         else:
+            discard """
             if i mod 100 == 0:
                 system.stdout.write("*")
                 flushFile(stdout)
+                """
             
 
 part2()
