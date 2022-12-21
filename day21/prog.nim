31df8b8c2860e57c0a45aa69911cf0c0e0049185
tony <tonetheman@gmail.com>
2022-12-21 12:22:35 -0600

    moving

--- day21/prog.nim
+++ day21/prog.nim
@@ -11,6 +11,9 @@ type
         sact : string
         val : int
         completed : bool
+        op : string
+        v1 : string
+        v2 : string
 
 proc insert_new_action(m : var Table[string,Action], input_string:string) =
     let mdata = input_string.split(":")
@@ -27,6 +30,12 @@ proc insert_new_action(m : var Table[string,Action], input_string:string) =
     m[mname] = Action(typ: mtyp, sact : mact, val: mval, completed: false)
     if mtyp == sInt:
         m[mname].completed = true
+    if mtyp == sAct:
+        let act = m[mname].sact.split(" ")
+        m[mname].op = act[1]
+        m[mname].v1 = act[0]
+        m[mname].v2 = act[2]
+
 
 proc resolve(name : string, m : var Table[string,Action]) =
     if m[name].completed:
@@ -37,6 +46,40 @@ proc resolve(name : string, m : var Table[string,Action]) =
     let op = act[1]
     let v1 = act[0]
     let v2 = act[2]
+
+    if m[v1].completed and m[v2].completed:
+        if op == "*":
+            m[name].val = m[v1].val * m[v2].val
+            m[name].completed = true
+        elif op == "-":
+            m[name].val = m[v1].val - m[v2].val
+            m[name].completed = true
+        elif op == "+":
+            m[name].val = m[v1].val + m[v2].val
+            m[name].completed = true
+        elif op == "/":
+            m[name].val = int(m[v1].val / m[v2].val)
+            m[name].completed = true
+
+proc resolve2(name : string, m : var Table[string,Action]) =
+    if m[name].completed:
+        return
+    if m[name].typ == sInt:
+        return
+    
+    # let act = m[name].sact.split(" ")
+    # let op = act[1]
+    # let v1 = act[0]
+    # let v2 = act[2]
+    let op = m[name].op
+    let v1 = m[name].v1
+    let v2 = m[name].v2
+
+    if name == "root":
+        if m[v1].completed and m[v2].completed:
+            if m[v1].val == m[v2].val:
+                m["root"].completed = true
+        return
 
     if m[v1].completed and m[v2].completed:
         if op == "*":
@@ -75,16 +118,44 @@ proc part1() =
 
 # part1()
 
+proc reset_completed(m : var Table[string,Action]) =
+    for name in m.keys:
+        if m[name].typ == sInt:
+            m[name].completed = true
+        else:
+            m[name].completed = false
+
 proc part2() =
-    let fdata = makefilebuffer("sample.txt")
+    let fdata = makefilebuffer("input.txt")
     var m : Table[string,Action]
     for s in fdata:
         insert_new_action(m,s)
     
     # adjust roots action
+    let ll = len(m)
 
     # change humn action to a number
+    for i in 20000 ..< 100000:
+        # echo("starting humn is: ", m["humn"])
+        m.reset_completed()
+        m["humn"].val = i
 
-    # eval and look for root equality match
+        # eval and look for root equality match
+        while m.countcompleted() < ll-1:
+
+            for singlemonkey in m.keys:
+                resolve2(singlemonkey,m)
+
+        # echo("root completion: ",m["root"].completed)
+        resolve2("root",m)
+        # echo("root completion: ",m["root"].completed)
+        if m["root"].completed == true:
+            echo("i is ",i)
+            break
+        else:
+            if i mod 100 == 0:
+                system.stdout.write("*")
+                flushFile(stdout)
+            
 
 part2()
